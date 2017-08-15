b123cb1cda7eea39666f503b391aa23842508d24
khituras <chew@gmx.net>
2017-08-15 15:52:35 +0200

    Minor change

--- src/main/java/de/julielab/java/utilities/FileUtilities.java
+++ src/main/java/de/julielab/java/utilities/FileUtilities.java
@@ -12,8 +12,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 

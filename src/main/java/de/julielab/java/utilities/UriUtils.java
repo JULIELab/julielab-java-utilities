f6d3643825375bec9f05d89d75d93c60be42c697
khituras <chew@gmx.net>
2018-04-04 16:43:34 +0200

    Added the UriUtils that currently automatically wrap URI streams into a GzipInputStream if the URI ends with .gzip or .gz.

---
+++ src/main/java/de/julielab/java/utilities/UriUtils.java
@@ -0,0 +1,17 @@
+package de.julielab.java.utilities;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.zip.GZIPInputStream;
+
+public class UriUtils {
+    public static BufferedInputStream getInputStreamFromUri(URI uri) throws IOException {
+        return new BufferedInputStream(uri.getPath().endsWith(".gz") || uri.getPath().endsWith(".gzip") ?
+                new GZIPInputStream(uri.toURL().openStream()) :
+                uri.toURL().openStream());
+    }
+
+}

d4583ef440a7e675698b4ea689188fb952a8e984
khituras <chew@gmx.net>
2018-08-06 12:26:26 +0200

    Release 1.0.3.

--- src/main/java/de/julielab/java/utilities/IOStreamUtilities.java
+++ src/main/java/de/julielab/java/utilities/IOStreamUtilities.java
@@ -10,10 +10,21 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IOStreamUtilities {
+    /**
+     * Returns a buffered reader with UTF-8 encoding from the given input stream.
+     * @param is The input stream to get an UTF-8 reader from.
+     * @return A UTF-8 encoding reader.
+     */
     public static BufferedReader getReaderFromInputStream(InputStream is) {
         return new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
     }
 
+    /**
+     * Interprets <tt>is</tt> as a stream of UTF-8 encoded lines and returns all lines.
+     * @param is The input stream.
+     * @return All UTF-8 encoded lines from the input stream.
+     * @throws IOException If reading fails.
+     */
     public static List<String> getLinesFromInputStream(InputStream is) throws IOException {
         try (BufferedReader br = getReaderFromInputStream(is)) {
             return br.lines().collect(Collectors.toList());

b4f5e134e51914a7780b5d0bcd5e6c9cf1357198
khituras <chew@gmx.net>
2019-02-27 12:59:16 +0100

    Adding a test from the jcore-gene-mapper that I forgot before.

---
+++ src/test/java/de/julielab/java/utilities/spanutils/OffsetSetTest.java
@@ -0,0 +1,46 @@
+package de.julielab.java.utilities.spanutils;
+
+import org.apache.commons.lang3.Range;
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class OffsetSetTest {
+
+	@Test
+	public void testLocate() {
+		OffsetSet offsets = new OffsetSet();
+		offsets.add(Range.between(0, 104));
+		offsets.add(Range.between(105, 286));
+		offsets.add(Range.between(287, 332));
+		
+		Range<Integer> result = offsets.locate(Range.between(105, 114));
+		Range<Integer> expected = Range.between(105, 286);
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void testLocateFirst() {
+		OffsetSet offsets = new OffsetSet();
+		offsets.add(Range.between(0, 104));
+		offsets.add(Range.between(105, 286));
+		offsets.add(Range.between(287, 332));
+		
+		Range<Integer> result = offsets.locate(Range.between(0, 5));
+		Range<Integer> expected = Range.between(0, 104);
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testComparator() {
+		OffsetSet offsets = new OffsetSet();
+		Comparator<? super Range<Integer>> c = offsets.comparator();
+		Range<Integer> firstRange = Range.between(287, 332);
+		Range<Integer> secondRange = Range.between(287, 332);
+		assertEquals(0, c.compare(firstRange,secondRange));
+	}
+
+}

981f6e4c89dcd6d0a02ee96f1ea45b1efab4c93c
khituras <chew@gmx.net>
2019-06-13 13:25:03 +0200

    Now closing the `InputStream` after converting it to a string.

--- src/main/java/de/julielab/java/utilities/IOStreamUtilities.java
+++ src/main/java/de/julielab/java/utilities/IOStreamUtilities.java
@@ -11,6 +11,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class IOStreamUtilities {
     /**
      * Returns a buffered reader with UTF-8 encoding from the given input stream.
+     *
      * @param is The input stream to get an UTF-8 reader from.
      * @return A UTF-8 encoding reader.
      */
@@ -20,6 +21,7 @@ public class IOStreamUtilities {
 
     /**
      * Interprets <tt>is</tt> as a stream of UTF-8 encoded lines and returns all lines.
+     *
      * @param is The input stream.
      * @return All UTF-8 encoded lines from the input stream.
      * @throws IOException If reading fails.
@@ -37,12 +39,13 @@ public class IOStreamUtilities {
      * as a buffer.
      * </p>
      * <p>Inspired by https://stackoverflow.com/a/48775964/1314955.</p>
+     *
      * @param is The <tt>InputStream</tt> to convert into a <tt>String</tt>.
      * @return The string contents of <tt>is</tt>.
      * @throws IOException If reading fails.
      */
-    public static String getStringFromInputStream(InputStream is) throws  IOException {
-        try(ByteArrayOutputStream result = new ByteArrayOutputStream()) {
+    public static String getStringFromInputStream(InputStream is) throws IOException {
+        try (ByteArrayOutputStream result = new ByteArrayOutputStream()) {
             byte[] buffer = new byte[8192];
             int length;
             while ((length = is.read(buffer)) != -1) {
@@ -50,6 +53,8 @@ public class IOStreamUtilities {
             }
 
             return result.toString(UTF_8);
+        } finally {
+            is.close();
         }
     }
 }
